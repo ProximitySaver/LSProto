38ca9b17940ed7bd9af051ed6720809e58e02a52
ProximitySaver <audric.ligtas@gmail.com>
2024-03-30 18:33:36 -0400

    Ajout de projet LinguoSphère dans projet GitHub

---
+++ LinguoSphèreProt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinguoSphèreProt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
