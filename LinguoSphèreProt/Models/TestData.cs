215cca8207c587a3c9791a6b197c2d5d31904355
ProximitySaver <audric.ligtas@gmail.com>
2024-03-31 14:35:38 -0400

    Création de classe TestData pour avoir des données de sample

---
+++ LinguoSphèreProt/Models/TestData.cs
@@ -0,0 +1,21 @@
+﻿namespace LinguoSphèreProt.Models
+{
+    public static class TestData
+    {
+        public static List<Niveau> GenerateNiveaux()
+        {
+            var niveaux = new List<Niveau>
+            {
+                new Niveau
+                {
+                    nom = "Beginner"
+                },
+                new Niveau
+                {
+                    nom = "Intermediate"
+                }
+            };
+            return niveaux;
+        }
+    }
+}
