570d012f6e087012c3cafb3c7d2692f4bc5a90fa
ProximitySaver <audric.ligtas@gmail.com>
2024-03-31 14:29:07 -0400

    Création de classe ApplicationDbContext pour intéragir avec la base de données

---
+++ LinguoSphèreProt/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LinguoSphèreProt.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Langage> Langages { get; set; }
+        public DbSet<Niveau> Niveaux { get; set; }
+
+    }
+}
